1693ff6e36360e5c7ec45eb236685e217684191e
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-28 22:28:47 +0530

    Middleware added for getting the request Method and Path

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
 app.get("/", (req, res) => res.send({ message: "Welcome to WEB3" }));
 
+// Start the server
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
