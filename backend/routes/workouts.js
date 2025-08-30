85b5bfda60cafccc142a959d145931d6810495d2
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-08-31 01:01:47 +0530

    added some functionality to run the db schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Workout } = require("../models/workoutModel");
 
 const router = express.Router();
 
@@ -13,8 +14,16 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
   res.json({ message: "POST a new workout" });
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // DELETE a workout by ID
