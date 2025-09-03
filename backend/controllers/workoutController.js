399da4709a0f55043320eb2fb0856ce793353a0f
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-09-03 23:34:23 +0530

    added a Controller to handle different request types

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,42 @@
+const { get } = require("http");
+
+const Workout = require("../models/workoutModel").Workout;
+
+// Get all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
+
+// Get a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const workout = await Workout.findById(id);
+    if (!workout) {
+      return res.status(404).json({ error: "No such workout" });
+    }
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: "Invalid ID" });
+  }
+};
+
+// Create a new workout
+const createWorkout = async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Delete a workout
+
+module.exports = {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+};
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const { Workout } = require("../models/workoutModel");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ message: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 // GET a single workout by ID
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post("/", async (req, res) => {
-  const { title, reps, load } = req.body;
-  try {
-    const workout = await Workout.create({ title, reps, load });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 // DELETE a workout by ID
 router.delete("/:id", (req, res) => {
