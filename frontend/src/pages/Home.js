b70b42d26bce1d5049e30e77c9242acb5d496f60
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-09-07 02:01:02 +0530

    added the fetch method for fetching the json data from the backend server

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("localhost:3010/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      } else {
+        console.log("error fetching workouts");
+      }
+    };
+
+    fetchWorkouts();
+  });
+
   return (
     <div className="home">
       <h1>Home Page</h1>
