65bc32a804d3d2e738fccd469c1836e0cf8e785a
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2025-09-11 23:43:21 +0530

    added a context component for wrapping up the App component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
+import { WorkoutContextProvider } from "./context/WorkoutContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WorkoutContextProvider>
+      <App />
+    </WorkoutContextProvider>
   </React.StrictMode>
 );
 
@@ -15,3 +18,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+/**
+ * WorkoutContextProvider works like a wrapper, that wraps the App component.
+ */
